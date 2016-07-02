6d2401c80293bf4ffd1ef758add30c9698bc22cd
nsteel <aceronicolas@hotmail.com>
2016-07-02 21:13:45 +0200

    Caltech lane detection was integrated with our project

--- src/cannyEdge.cpp
+++ src/cannyEdge.cpp
@@ -7,19 +7,14 @@
 void CannyEdge::preprocess(cv::Mat& img) {
         SWRI_PROFILE("Canny");
 
-        img = img(cv::Rect(0, img.rows*config.horizon_percentage/100, img.cols*0.7, img.rows*(1-config.horizon_percentage/100)));
-
-        //cv::Mat gradient_x;
-        //cv::Mat gradient_y;
-        //cv::Mat sobel_norm;
-
+        //img = img(cv::Rect(0, img.rows*config.horizon_percentage/100, img.cols*0.7, img.rows*(1-config.horizon_percentage/100)));
         cv::normalize(img, img, 0, 255, cv::NORM_MINMAX, CV_8UC1);
-        cv::blur(img , img, cv::Size(3,3));
+        cv::blur(img , img, cv::Size(2,2));
         //cv::dilate(img, img, cv::Mat());
-        cv::threshold(img, img, 180, 255, CV_THRESH_BINARY);
+        cv::threshold(img, img, 170, 255, CV_THRESH_BINARY);
 
-        cv::Canny(img,img, 180, 300);
-        cv::dilate(img, img, cv::Mat());
+        cv::Canny(img,img, 170, 300);
+        //cv::dilate(img, img, cv::Mat());
 
         std::vector<std::vector<cv::Point> > detectedEdges;
         std::vector<cv::Vec4i> hierarchy;
--- src/lane_detector.cpp
+++ src/lane_detector.cpp
@@ -14,8 +14,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <lane_detector/cannyEdge.h>
-#include <lane_detector/houghTransform.h>
-#include <lane_detector/fittingApproach.h>
 #include <cv.h>
 #include <sstream>
 #include <dynamic_reconfigure/server.h>
@@ -24,64 +22,220 @@
 #include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/PolygonStamped.h>
 #include <lane_detector/ipm.h>
+#include <lane_detector/LaneDetector.hh>
+#include <lane_detector/CameraInfoOpt.h>
+#include <lane_detector/LaneDetectorOpt.h>
+#include <lane_detector/mcv.hh>
 
 cv_bridge::CvImagePtr currentFrame_ptr;
 Preprocessor* preproc;
-FeatureExtractor<linesPair>* extractor;
-Fitting<linesPair>* fittingPhase;
 image_transport::Publisher resultImg_pub;
 ros::Publisher detectedPoints_pub;
 lane_detector::DetectorConfig dynConfig;
+LaneDetector::CameraInfo cameraInfo;
+LaneDetector::LaneDetectorConf lanesConf;
+
+
+void setConfig() {
+
+  //init the strucure
+  lanesConf.ipmWidth = dynConfig.ipmWidth;
+  lanesConf.ipmHeight = dynConfig.ipmHeight;
+  lanesConf.ipmLeft = dynConfig.ipmLeft;
+  lanesConf.ipmRight = dynConfig.ipmRight;
+  lanesConf.ipmBottom = dynConfig.ipmBottom;
+  lanesConf.ipmTop = dynConfig.ipmTop;
+  lanesConf.ipmInterpolation = dynConfig.ipmInterpolation;
+
+  lanesConf.lineWidth = dynConfig.lineWidth;
+  lanesConf.lineHeight = dynConfig.lineHeight;
+  lanesConf.kernelWidth = dynConfig.kernelWidth;
+  lanesConf.kernelHeight = dynConfig.kernelHeight;
+  lanesConf.lowerQuantile =
+      dynConfig.lowerQuantile;
+  lanesConf.localMaxima =
+      dynConfig.localMaxima;
+  lanesConf.groupingType = dynConfig.groupingType;
+  lanesConf.binarize = dynConfig.binarize;
+  lanesConf.detectionThreshold =
+      dynConfig.detectionThreshold;
+  lanesConf.smoothScores =
+      dynConfig.smoothScores;
+  lanesConf.rMin = dynConfig.rMin;
+  lanesConf.rMax = dynConfig.rMax;
+  lanesConf.rStep = dynConfig.rStep;
+  lanesConf.thetaMin = dynConfig.thetaMin * CV_PI/180;
+  lanesConf.thetaMax = dynConfig.thetaMax * CV_PI/180;
+  lanesConf.thetaStep = dynConfig.thetaStep * CV_PI/180;
+  lanesConf.ipmVpPortion = dynConfig.ipmVpPortion;
+  lanesConf.getEndPoints = dynConfig.getEndPoints;
+  lanesConf.group = dynConfig.group;
+  lanesConf.groupThreshold = dynConfig.groupThreshold;
+  lanesConf.ransac = dynConfig.ransac;
+
+  lanesConf.ransacLineNumSamples = dynConfig.ransacLineNumSamples;
+  lanesConf.ransacLineNumIterations = dynConfig.ransacLineNumIterations;
+  lanesConf.ransacLineNumGoodFit = dynConfig.ransacLineNumGoodFit;
+  lanesConf.ransacLineThreshold = dynConfig.ransacLineThreshold;
+  lanesConf.ransacLineScoreThreshold = dynConfig.ransacLineScoreThreshold;
+  lanesConf.ransacLineBinarize = dynConfig.ransacLineBinarize;
+  lanesConf.ransacLineWindow = dynConfig.ransacLineWindow;
+
+  lanesConf.ransacSplineNumSamples = dynConfig.ransacSplineNumSamples;
+  lanesConf.ransacSplineNumIterations = dynConfig.ransacSplineNumIterations;
+  lanesConf.ransacSplineNumGoodFit = dynConfig.ransacSplineNumGoodFit;
+  lanesConf.ransacSplineThreshold = dynConfig.ransacSplineThreshold;
+  lanesConf.ransacSplineScoreThreshold = dynConfig.ransacSplineScoreThreshold;
+  lanesConf.ransacSplineBinarize = dynConfig.ransacSplineBinarize;
+  lanesConf.ransacSplineWindow = dynConfig.ransacSplineWindow;
+
+  lanesConf.ransacSplineDegree = dynConfig.ransacSplineDegree;
+
+  lanesConf.ransacSpline = dynConfig.ransacSpline;
+  lanesConf.ransacLine = dynConfig.ransacLine;
+  lanesConf.ransacSplineStep = dynConfig.ransacSplineStep;
+
+  lanesConf.overlapThreshold = dynConfig.overlapThreshold;
+
+  lanesConf.localizeAngleThreshold = dynConfig.localizeAngleThreshold;
+  lanesConf.localizeNumLinePixels = dynConfig.localizeNumLinePixels;
+
+  lanesConf.extendAngleThreshold = dynConfig.extendAngleThreshold;
+  lanesConf.extendMeanDirAngleThreshold = dynConfig.extendMeanDirAngleThreshold;
+  lanesConf.extendLinePixelsTangent = dynConfig.extendLinePixelsTangent;
+  lanesConf.extendLinePixelsNormal = dynConfig.extendLinePixelsNormal;
+  lanesConf.extendContThreshold = dynConfig.extendContThreshold;
+  lanesConf.extendDeviationThreshold = dynConfig.extendDeviationThreshold;
+  lanesConf.extendRectTop = dynConfig.extendRectTop;
+  lanesConf.extendRectBottom = dynConfig.extendRectBottom;
+
+  lanesConf.extendIPMAngleThreshold = dynConfig.extendIPMAngleThreshold;
+  lanesConf.extendIPMMeanDirAngleThreshold = dynConfig.extendIPMMeanDirAngleThreshold;
+  lanesConf.extendIPMLinePixelsTangent = dynConfig.extendIPMLinePixelsTangent;
+  lanesConf.extendIPMLinePixelsNormal = dynConfig.extendIPMLinePixelsNormal;
+  lanesConf.extendIPMContThreshold = dynConfig.extendIPMContThreshold;
+  lanesConf.extendIPMDeviationThreshold = dynConfig.extendIPMDeviationThreshold;
+  lanesConf.extendIPMRectTop = dynConfig.extendIPMRectTop;
+  lanesConf.extendIPMRectBottom = dynConfig.extendIPMRectBottom;
+
+  lanesConf.splineScoreJitter = dynConfig.splineScoreJitter;
+  lanesConf.splineScoreLengthRatio = dynConfig.splineScoreLengthRatio;
+  lanesConf.splineScoreAngleRatio = dynConfig.splineScoreAngleRatio;
+  lanesConf.splineScoreStep = dynConfig.splineScoreStep;
+
+  lanesConf.splineTrackingNumAbsentFrames = dynConfig.splineTrackingNumAbsentFrames;
+  lanesConf.splineTrackingNumSeenFrames = dynConfig.splineTrackingNumSeenFrames;
+
+  lanesConf.mergeSplineThetaThreshold = dynConfig.mergeSplineThetaThreshold;
+  lanesConf.mergeSplineRThreshold = dynConfig.mergeSplineRThreshold;
+  lanesConf.mergeSplineMeanThetaThreshold = dynConfig.mergeSplineMeanThetaThreshold;
+  lanesConf.mergeSplineMeanRThreshold = dynConfig.mergeSplineMeanRThreshold;
+  lanesConf.mergeSplineCentroidThreshold = dynConfig.mergeSplineCentroidThreshold;
+
+  lanesConf.lineTrackingNumAbsentFrames = dynConfig.lineTrackingNumAbsentFrames;
+  lanesConf.lineTrackingNumSeenFrames = dynConfig.lineTrackingNumSeenFrames;
+
+  lanesConf.mergeLineThetaThreshold = dynConfig.mergeLineThetaThreshold;
+  lanesConf.mergeLineRThreshold = dynConfig.mergeLineRThreshold;
+
+  lanesConf.numStrips = dynConfig.numStrips;
+
+
+  lanesConf.checkSplines = dynConfig.checkSplines;
+  lanesConf.checkSplinesCurvenessThreshold = dynConfig.checkSplinesCurvenessThreshold;
+  lanesConf.checkSplinesLengthThreshold = dynConfig.checkSplinesLengthThreshold;
+  lanesConf.checkSplinesThetaDiffThreshold = dynConfig.checkSplinesThetaDiffThreshold;
+  lanesConf.checkSplinesThetaThreshold = dynConfig.checkSplinesThetaThreshold;
+
+  lanesConf.checkIPMSplines = dynConfig.checkIPMSplines;
+  lanesConf.checkIPMSplinesCurvenessThreshold = dynConfig.checkIPMSplinesCurvenessThreshold;
+  lanesConf.checkIPMSplinesLengthThreshold = dynConfig.checkIPMSplinesLengthThreshold;
+  lanesConf.checkIPMSplinesThetaDiffThreshold = dynConfig.checkIPMSplinesThetaDiffThreshold;
+  lanesConf.checkIPMSplinesThetaThreshold = dynConfig.checkIPMSplinesThetaThreshold;
+
+  lanesConf.finalSplineScoreThreshold = dynConfig.finalSplineScoreThreshold;
+
+  lanesConf.useGroundPlane = dynConfig.useGroundPlane;
+
+  lanesConf.checkColor = dynConfig.checkColor;
+  lanesConf.checkColorNumBins = dynConfig.checkColorNumBins;
+  lanesConf.checkColorWindow = dynConfig.checkColorWindow;
+  lanesConf.checkColorNumYellowMin = dynConfig.checkColorNumYellowMin;
+  lanesConf.checkColorRGMin = dynConfig.checkColorRGMin;
+  lanesConf.checkColorRGMax = dynConfig.checkColorRGMax;
+  lanesConf.checkColorGBMin = dynConfig.checkColorGBMin;
+  lanesConf.checkColorRBMin = dynConfig.checkColorRBMin;
+  lanesConf.checkColorRBFThreshold = dynConfig.checkColorRBFThreshold;
+  lanesConf.checkColorRBF = dynConfig.checkColorRBF;
+
+  lanesConf.ipmWindowClear = dynConfig.ipmWindowClear;
+  lanesConf.ipmWindowLeft = dynConfig.ipmWindowLeft;
+  lanesConf.ipmWindowRight = dynConfig.ipmWindowRight;
+
+  lanesConf.checkLaneWidth = dynConfig.checkLaneWidth;
+  lanesConf.checkLaneWidthMean = dynConfig.checkLaneWidthMean;
+  lanesConf.checkLaneWidthStd = dynConfig.checkLaneWidthStd;
+
+  mcvInitCameraInfo("/home/n/lane-detector/src/CameraInfo.conf", &cameraInfo);
+
+}
 
 void configCallback(lane_detector::DetectorConfig& config, uint32_t level)
 {
-        preproc->setConfig(config);
-        extractor->setConfig(config);
-        fittingPhase->setConfig(config);
+        //preproc->setConfig(config);
         dynConfig = config;
+        setConfig();
         ROS_DEBUG("Config was set");
 }
 
-template<class T>
-void processImage(Preprocessor* preproc, FeatureExtractor<T>* extractor, Fitting<T>* fittingPhase) {
+void processImage(LaneDetector::CameraInfo& cameraInfo, LaneDetector::LaneDetectorConf& lanesConf) {
 
-        if(currentFrame_ptr->image.channels() == 3) {
-                cv::cvtColor(currentFrame_ptr->image, currentFrame_ptr->image, CV_BGR2GRAY);
-        }
+  if(currentFrame_ptr) {
 
-        cv::Mat copyImg;
-        linesPair features;
-        IPM ipm;
-        ipm.applyHomography(currentFrame_ptr->image, currentFrame_ptr->image);
-        //cv::imshow("IPM", currentFrame_ptr->image);
-        //cv::waitKey(20);
-        currentFrame_ptr->image.copyTo(copyImg);
-        cv::cvtColor(copyImg, copyImg, CV_GRAY2BGR);
-        preproc->preprocess(currentFrame_ptr->image);
-        //extractor->extract(copyImg, currentFrame_ptr->image, features);
-        //std::vector<cv::Point> points = fittingPhase->fitting(copyImg, currentFrame_ptr->image, features);
+    // detect lanes
+    std::vector<float> lineScores, splineScores;
+    std::vector<LaneDetector::Line> lanes;
+    std::vector<LaneDetector::Spline> splines;
+    std::vector<LaneDetector::Spline> splines_world;
+    cv::Mat originalImg = currentFrame_ptr->image;
+    //cv::Mat originalImg = cv::imread("/home/n/Desktop/curve.png");
+    CvMat raw_mat = originalImg;
+    CvMat* raw_ptr = &raw_mat;
+    CvMat* mat_ptr;
+    LaneDetector::mcvLoadImage(&raw_ptr, &mat_ptr);
+    if(dynConfig.debug_lines) LaneDetector::DEBUG_LINES = 1;
+    //LaneDetector::SHOW_IMAGE(&mat, "HOla", 1);
+    mcvGetLanes(mat_ptr, raw_ptr, &lanes, &lineScores, &splines, &splines_world, &splineScores,
+                &cameraInfo, &lanesConf, NULL);
 
-        if(currentFrame_ptr->image.channels() == 1)
+       // print lanes
+        for(int i=0; i<splines.size(); i++)
+         {
+           if (splines[i].color == LaneDetector::LINE_COLOR_YELLOW)
+             mcvDrawSpline(raw_ptr, splines[i], CV_RGB(255,255,0), 3);
+           else
+             mcvDrawSpline(raw_ptr, splines[i], CV_RGB(0,255,0), 3);
+         }
+
+        std::cout << "frame#" << setw(8) << setfill('0') << 0 <<
+          " has " << splines_world.size() << " splines" << endl;
+        for (int i=0; i<splines_world.size(); ++i)
         {
-                cv::cvtColor(currentFrame_ptr->image, currentFrame_ptr->image, CV_GRAY2BGR);
+          std::cout << "\tspline#" << i+1 << " has " <<
+            splines_world[i].degree+1 << " points and score " <<
+            splineScores[i] << endl;
+          for (int j=0; j<=splines_world[i].degree; ++j)
+            std::cout<< "\t\t" <<
+              splines_world[i].points[j].x << ", " <<
+              splines_world[i].points[j].y << endl;
+          char str[256];
+          sprintf(str, "%d", i);
+          LaneDetector::mcvDrawText(raw_ptr, str,
+                      cvPointFrom32f(splines[i].points[splines[i].degree]),
+                                      1, CV_RGB(0, 0, 255));
         }
-        /*geometry_msgs::Point32 vanishingPoint;
-        vanishingPoint.x = points[0].x;
-        vanishingPoint.y = points[0].y;
-
-        geometry_msgs::Point32 left_lane;
-        left_lane.x = points[1].x;
-        left_lane.y = points[1].y;
-
-        geometry_msgs::Point32 right_lane;
-        right_lane.x = points[2].x;
-        right_lane.y = points[2].y;
-
-        geometry_msgs::PolygonStamped detectedPoints;
-        detectedPoints.header.stamp = ros::Time::now(); // time
-        detectedPoints.header.frame_id = "base_footprint";
-        detectedPoints.polygon.points = {vanishingPoint, left_lane, right_lane};
-        detectedPoints_pub.publish(detectedPoints);*/
+        cvReleaseMat(&mat_ptr);
+  }
 }
 
 void readImg(const sensor_msgs::ImageConstPtr& img)
@@ -90,11 +244,12 @@ void readImg(const sensor_msgs::ImageConstPtr& img)
         try
         {
                 currentFrame_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
-                processImage(preproc, extractor, fittingPhase);
-                if(dynConfig.image != 0) {
-                        currentFrame_ptr->encoding = sensor_msgs::image_encodings::MONO8;
-                }
+                processImage(cameraInfo, lanesConf);
+                //if(dynConfig.image != 0) {
+                  //      currentFrame_ptr->encoding = sensor_msgs::image_encodings::MONO8;
+                //}
                 resultImg_pub.publish(*currentFrame_ptr->toImageMsg());
+                currentFrame_ptr.reset();
         }
         catch (cv_bridge::Exception& e)
         {
@@ -108,9 +263,7 @@ void readImg(const sensor_msgs::ImageConstPtr& img)
 int main(int argc, char **argv){
 
         preproc = new CannyEdge();
-        extractor =  new HoughTransform();
-        fittingPhase = new FittingApproach();
-
+        //processImage(cameraInfo, lanesConf);
         ros::init(argc, argv, "lane_detector");
 
         /**
@@ -149,8 +302,9 @@ int main(int argc, char **argv){
         resultImg_pub = it.advertise("lane_detector/result", 1);
         detectedPoints_pub = nh.advertise<geometry_msgs::PolygonStamped>("lane_detector/vanishing_point", 1);
 
-        ros::MultiThreadedSpinner spinner(0); // Use one thread for core
-        spinner.spin(); // spin() will not return until the node has been shutdown
-        //ros::spin();
+        //ros::MultiThreadedSpinner spinner(0); // Use one thread for core
+        //spinner.spin(); // spin() will not return until the node has been shutdown
+        ros::spin();
+        delete preproc;
         return 0;
 }
